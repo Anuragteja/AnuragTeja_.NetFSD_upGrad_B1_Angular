d8a611f48bc564a4dfbbb66ff9b46deb39d7009d
K.ANURAG TEJA <120965320+Anuragteja@users.noreply.github.com>
2026-02-24 09:19:09 +0530

    Add files via upload

---
+++ JavaScript/Assignment1_solutions/Assignment2.js
@@ -0,0 +1,15 @@
+let Marks =[99,80,33,75,60,61];
+for (let i of Marks){
+if(i>=75){
+console.log("Grade A");
+}
+else if(i>=60){
+console.log("Grade B");
+}
+else if(i>=40){
+console.log("Grade C");
+}
+else{
+console.log("Fail");
+}
+}
