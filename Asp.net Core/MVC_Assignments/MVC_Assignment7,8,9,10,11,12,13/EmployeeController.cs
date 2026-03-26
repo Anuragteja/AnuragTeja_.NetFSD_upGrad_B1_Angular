3c37fbfff4d0087c95fea5ce6d515fc4b10095fc
K.ANURAG TEJA <120965320+Anuragteja@users.noreply.github.com>
2026-03-26 16:52:24 +0530

    Add files via upload

---
+++ Asp.net Core/MVC_Assignments/MVC_Assignment7,8,9,10,11,12,13/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment7.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Details(string name, int salary, string Department)
+        {
+            return $"name is {name} salary is :{salary} department is {Department}";
+        }
+    }
+}
---
+++ Asp.net Core/MVC_Assignments/MVC_Assignment7,8,9,10,11,12,13/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment7.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string name,int age)
+        {
+            ViewData["Name"] = name;
+            ViewData["age"]= age;
+            return View();
+        }
+
+        public IActionResult Info(string name, int age)
+        {
+            ViewData["Name"] = "john";
+            ViewData["age"] = 102;
+            return View();
+        }
+
+        public IActionResult Titles(string name)
+        {
+            ViewData["Title"] = name;
+            return View();
+        }
+
+        public IActionResult students(string name)
+        {
+            ViewData["names"] = new List<string>
+                {
+                    "Anu",
+                    "Ravi",
+                    "Sneha"
+                };
+
+            return View();
+        }
+    }
+}
